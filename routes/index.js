b5105ded7686ab01625fe14d2a1b7dfec409b87e
nicomellon <nmellon94@gmail.com>
2021-11-15 10:08:47 +0100

    labs done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,41 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+const mongoose = require("mongoose");
+const Movie = require("../models/Movie.model");
+const MONGO_URI = process.env.MONGO_URI;
+
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", async (req, res) => {
+  try {
+    mongoose.connect(MONGO_URI, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    const movies = await Movie.find();
+    res.render("movies/movies-list", { movies });
+    mongoose.disconnect();
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+router.get("/movie/:id", async (req, res) => {
+  try {
+    mongoose.connect(MONGO_URI, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    const movieInfo = await Movie.findById(req.params.id);
+    res.render("movies/movie-info", { movieInfo });
+    mongoose.disconnect();
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 module.exports = router;
